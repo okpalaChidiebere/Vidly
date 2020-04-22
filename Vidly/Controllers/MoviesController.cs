637d7bbd4650fa0c381909261e063038dce1edca
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-04-21 20:13:39 -0600

    Sample MVC in Action
    Assume we want to create a page were we randomly want to pick a movie
      and render its details. The page will be /movies/random
    
    created a movie model, controller and view.
    
    MoviesController.cs:
    * Movie.cs:
    * Random.cshtml:

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;  //refers to our 'Movie' model class
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller //moviesController derived from the contoller class
+    {
+        //GET: /Movies/Random
+        public ActionResult Random()  //we have an action called 'Random' that returns action result
+        {
+            /*create a new instance of movie.*/
+            var movie = new Movie() { Name = "Shrek!"}; //in real life we will get this values from a web API or database
+
+            return View(movie);  //return the View called Random. it should be in the View folder. so we go create the view
+        }
+    }
+}
---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Vidly.Models
+{
+
+    /*THis represents thestae of our application in terms of the problem domain*/
+    public class Movie
+    {
+
+        //properties
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        
+        //movies/random  -> means a crontroller named movies with an action called random
+
+    }
+}
